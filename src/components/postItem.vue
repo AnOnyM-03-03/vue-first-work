88eeae6f3aa8fbb1c071c754fb71d910e3e4a4fb
Tolstov <tolstov.dmitrij42@gmail.com>
2022-06-14 19:55:50 +0400

    separate instance for posts

---
+++ src/components/postItem.vue
@@ -0,0 +1,47 @@
+<template>
+     <div class="post">
+        <div>
+            <div><strong>Name:</strong> {{post.name}}</div>
+            <div><strong>Age:</strong>{{post.age}}</div>
+        </div>
+        <div class="post-buttons">
+            <my-button
+            @click="$emit('remove', post)"
+            >
+            Удалить
+            </my-button>  
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props:{
+        post:{
+            type: Object,
+            required: true
+
+        }
+    },
+
+data(){
+
+return{ }
+
+},
+
+}
+
+</script>
+
+<style>
+.post{
+    padding: 15px;
+    border: 3px solid #c0c00f;
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+}
+</style>
