ed02563319d563f6356047559080ef3d05cecbb9
Tolstov <tolstov.dmitrij42@gmail.com>
2022-06-14 20:05:32 +0400

    creating an input component for reuse

---
+++ src/UI/myInput.vue
@@ -0,0 +1,35 @@
+<template>
+    <input :value="modelValue" @input="updateInput" class="input" type="text">
+</template>
+
+<script>
+
+export default{
+    name:'my-input',
+    props:{
+        modelValue:[String, Number]
+    },
+
+data(){
+
+return{ }
+
+},
+methods:{
+    updateInput(e){
+        this.$emit('update:modelValue', e.target.value)
+    }
+}
+
+}
+
+</script>
+
+<style>
+.input{
+    width: 100%;
+    border: 3px solid #03c292;
+    padding: 10px;
+    margin-top: 10px;
+}
+</style>
