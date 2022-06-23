2b735d66929c4fd36ff46b84d531840599f8f84d
Tolstov <tolstov.dmitrij42@gmail.com>
2022-06-23 20:19:46 +0400

    instantiating search by attribute

---
+++ src/UI/mySelect.vue
@@ -0,0 +1,37 @@
+<template>
+<select v-model="modelValue" @change="changeOption">
+    <option disabled value="">Выберите из списка</option>
+    <option 
+    v-for="option in options" 
+    :key="option.value" 
+    :value="option.value"> {{ option.title }}</option>
+</select>
+
+</template>
+
+<script>
+
+export default{
+    name:'my-select',
+    props:{
+        modelValue:{
+            type: String
+        },
+        options:{
+            type: Array,
+            default: () =>[]
+        }
+    },
+methods:{
+    changeOption(e){
+        this.$emit('update:modelValue', e.target.value)
+    }
+}
+
+}
+
+</script>
+
+<style>
+
+</style>
