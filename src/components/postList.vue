891120b71ba3558038a817744b56a514e41b2519
Tolstov <tolstov.dmitrij42@gmail.com>
2022-06-23 20:21:35 +0400

    iterating posts and creating styles

--- src/components/postList.vue
+++ src/components/postList.vue
@@ -3,14 +3,15 @@
 
           <h3>Список пользователей:</h3>
 
-          <post-item class="post" 
-          v-for="post in posts" 
-          :key="post.id"
-          :post="post"
-          @remove="$emit('remove', post)"
+          <transition-group name="user-list">
+            <post-item class="post" 
+                v-for="post in posts" 
+                :key="post.id"
+                :post="post"
+                @remove="$emit('remove', post)"
 
-          /> 
-
+            />
+          </transition-group>
         </div>
 
         <h2 v-else style="color:red">Список пуст</h2>
@@ -33,5 +34,20 @@ export default{
 </script>
 
 <style scoped>
-
+.user-list-item{
+    display: inline-block;
+    margin-right: 10px;
+} 
+.user-list-enter-active,
+.user-list-leave-active {
+  transition: all 0.4s ease-in-out;
+}
+.user-list-enter-from,
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.user-list-move {
+  transition: transform 0.8s ease;
+}
 </style>
