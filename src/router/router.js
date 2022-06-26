81cf3c5f712f2e5585e05f7dd1a5bf6143b89772
Tolstov <tolstov.dmitrij42@gmail.com>
2022-06-26 20:32:56 +0400

    creating a path to post description

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,6 @@
 import mainPage from '@/pages/mainPage';
 import postPage from '@/pages/postPage';
+import postIdPage from '@/pages/postIdPage';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
@@ -10,6 +11,10 @@ const routes = [
    {
       path: '/post',
       component: postPage,
+   },
+   {
+      path: '/post/:id',
+      component: postIdPage,
    },
 ];
 
