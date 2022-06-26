73453edd60bb401c337adaeef338bc186648c527
Tolstov <tolstov.dmitrij42@gmail.com>
2022-06-26 20:19:10 +0400

    creating a site navigation block

---
+++ src/UI/navBar.vue
@@ -0,0 +1,44 @@
+<template>
+
+<div class="navBar">
+    <div class="home" @click="$router.push('/')">Home</div>
+    <div class="nav-bar__btn">
+        <my-button @click="$router.push('/post')">posts</my-button>
+    </div>
+</div>
+
+</template>
+
+<script>
+
+export default{
+
+data(){
+
+return{ }
+
+},
+
+}
+
+</script>
+
+<style scoped>
+.navBar{
+    height: 50px;
+    background-color: teal;
+    box-shadow: 2px 2px 4px black;
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+
+}
+.nav-bar__btn{
+    margin-left: auto;
+}
+.home{
+    cursor: pointer;
+    font-size: 20px;
+    color: #fff;
+}
+</style>
